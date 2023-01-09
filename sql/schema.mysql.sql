ed847a13438e032340fb23fd0eb3243ceb08e7d9
Sanjay Soundarajan <ssoundarajan@calmi2.org>
2023-01-08 22:09:12 -0800

    feat!: update to v1.39.5 (#175)

--- sql/schema.mysql.sql
+++ sql/schema.mysql.sql
@@ -75,5 +75,4 @@ CREATE TABLE `website` (
     PRIMARY KEY (`website_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-
 INSERT INTO account (username, password, is_admin) values ('admin', '$2b$10$BUli0c.muyCW1ErNJc3jL.vFRFtFJWrT8/GcR4A.sUdCznaXiqFXa', true);
